00d3a0395598931b4e7cde2896cfd8211fc3be12
unknown <nazmussakib0501200211@gmail.com>
2025-01-25 12:54:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    <div className="p-4">
+      <button className="btn btn-primary">Primary Button</button>
+      <button className="btn btn-secondary ml-2">Secondary Button</button>
+    </div>
+  
+
+    </>
+  )
+}
+
+export default App
